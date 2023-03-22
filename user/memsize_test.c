4ede0fe9d84a29349f0b7a338631cf1af983c689
RoiTief <roitief@post.bgu.ac.il>
2023-03-22 21:44:41 +0200

    task2 completed

---
+++ user/memsize_test.c
@@ -0,0 +1,14 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main()
+{
+	void* p;
+	printf("the size of the process now is : %d \nallocating 20k bytes\n", memsize());
+	p =malloc(20000);
+	printf("the size of the process now is : %d \nfreeing the pointer\n", memsize());
+	free(p); 
+	printf("the size of the process now is : %d\n", memsize());
+	exit(0);
+}
